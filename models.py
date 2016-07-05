8a82dc9a84055e63221e31d6f4fde240582608c2
Apophus <bkilel12@gmail.com>
2016-07-05 19:09:19 +0300

    models to create users

--- models.py
+++ models.py
@@ -1,11 +1,14 @@
 #!usr/bin/python
 
 import datetime
+
+from flask_bcrypt import generate_password_hash
+from flask.ext.login import UserMixin
 from peewee import *
 
 DATABASE = PostgresDatabase('social.db')
 
-class User(Model):
+class User(UserMixin, Model):
     username = CharField(unique=True)
     email = CharField(unique=True)
     password = CharField(max_length=100)
@@ -16,3 +19,14 @@ class meta:
     database = DATABASE
     order_by = ('joined_at')
 
+@classmethod
+def create_user(cls, username, email, password, admin=False):
+    try:
+        cls.create(
+            username=username,
+            password=generate_password_hash(password),
+            email=email,
+            is_admin=admin
+        )
+    except IntegrityError:
+        raise ValueError('User already exists')
