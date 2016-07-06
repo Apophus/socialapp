34556995be91cc321affddcba437ec18311e88d3
Apophus <bkilel12@gmail.com>
2016-07-06 06:19:46 +0300

    created the app

---
+++ app.py
@@ -0,0 +1,51 @@
+#!usr/bin/python
+
+from flask import Flask, g
+from flask.ext.login import LoginManager
+
+import models
+
+DEBUG = True
+PORT = 8000
+HOST='0.0.0.0'
+
+app = Flask(__name__)
+app.secret_key ='hard to guess'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    try
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+         return None
+
+
+@app.before_request()
+def before_request():
+    #connect to database before each request
+    g.db =models.DATABASE
+    g.db.connect()
+
+
+@app.after_request
+def after_request():
+    #close the database after each request
+    g.db.close()
+    return response
+
+
+if __name__ == '__main__':
+    models.initialize()
+    models.User.create_user(
+        name="Larrisa",
+        email="larrisa@gmail.com",
+        password='password',
+        admin=True
+    )
+
+    app.run(debug=DEBUG, port=PORT, host=HOST)
+
